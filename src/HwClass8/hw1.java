8263cca1ab3031b348928f8cc38d5b2e40601057
MikeZ1189 <zmich89@vt.edu>
2023-01-14 16:22:39 -0500

    homework

---
+++ src/HwClass8/hw1.java
@@ -0,0 +1,14 @@
+package HwClass8;
+
+public class hw1 {
+    public static void main(String[] args) {
+        int num = 0;
+        while (num <= 50) {
+            if (num % 3!= 0) {
+                System.out.println(num);
+            }
+            num++;
+        }
+    }
+}
+
